adcce38f8baaee53bef05ca1c7204b1dc5b80fd2
andymatsuura <armatsuura@gmail.com>
2024-04-08 20:03:44 -0700

    add working map, new graphs

--- map.js
+++ map.js
@@ -8,9 +8,9 @@ d3.json(url).then(function(data) {
 function chargerNumber(feature) {
     let result = [];
 
-    const level1Charger = feature.properties.ev_level1_evse_num || 0;
-    const level2Charger = feature.properties.ev_level2_evse_num || 0;
-    const dcCharger = feature.properties.ev_dc_fast_num || 0;
+    const level1Charger = feature.properties.ev_level1_evse_num;
+    const level2Charger = feature.properties.ev_level2_evse_num;
+    const dcCharger = feature.properties.ev_dc_fast_num;
     
     if (level1Charger === null && level2Charger === null && dcCharger === null) {
         result.push(null);
@@ -29,20 +29,17 @@ function chargerNumber(feature) {
     } else {
         result.push(level1Charger + level2Charger + dcCharger);
     }
-
     return result
+    
 }
 
-function markerColor(feature) {
-    const chargers = chargerNumber(feature);
-
-    if (chargers === null) return "black"; // No chargers available
-
+function markerColor(chargers) {
     // Define color thresholds based on the number of chargers
-    if (chargers >= 25) return "green"; // High availability
-    else if (chargers >= 15) return "orange"; // Medium availability
-    else if (chargers >= 5) return "#66ff00"; // Low availability
-    else return "##ff0000"; // Very low availability
+    if (chargers < 0) return "red"; 
+    else if (chargers < 5) return "red"; 
+    else if (chargers < 15) return "orange"; 
+    else if (chargers < 25) return  "yellow"; 
+    else return "green"
 };
 
 function createFeatures(electricVehicleStations) {
@@ -50,13 +47,25 @@ function createFeatures(electricVehicleStations) {
         layer.bindPopup(`<h3>Name: ${feature.properties.station_name}</h3><hr>
             <p>Address: ${feature.properties.street_address}</p><hr><p>Network: ${feature.properties.ev_network}</p><hr><p>Accessibility: ${feature.properties.access_days_time}</p>`);
     }
+    var centerLatLng = myMap.getCenter(); // get map center
+    var pointC = myMap.latLngToContainerPoint(centerLatLng); // convert to containerpoint (pixels)
+    var pointX = [pointC.x + 1, pointC.y]; // add one pixel to x
+    var pointY = [pointC.x, pointC.y + 1]; // add one pixel to y
+
+    // convert containerpoints to latlng's
+    var latLngC = map.containerPointToLatLng(pointC);
+    var latLngX = map.containerPointToLatLng(pointX);
+    var latLngY = map.containerPointToLatLng(pointY);
+
+    var distanceX = latLngC.distanceTo(latLngX); // calculate distance between c and x (latitude)
+    var distanceY = latLngC.distanceTo(latLngY); // calculate distance between c and y (longitude)
 
     let stations = L.geoJSON(electricVehicleStations, {
         onEachFeature: onEachFeature,
         pointToLayer: function (feature, latlng) {
             let markers = {
-                radius: 10,
-                fillColor: markerColor(feature),
+                radius: 210 / distanceX,
+                fillColor: markerColor(chargerNumber(feature)),
                 weight: 1,
                 opacity: 1, 
                 color: "white",
@@ -101,15 +110,20 @@ function createMap(stations) {
     let legend = L.control({position: "bottomright"});
     legend.onAdd = function() {
         let div = L.DomUtil.create("div", "info legend");
-        let chargerAmount = [25, 15, 5, 0]; 
+        let chargerAmount = [0, 5, 15, 25]; 
     
-        div.innerHTML += "<h3>Charger Number</h3><hr><p>Radius: 210 miles</p>";
+        div.innerHTML += "<h3>Charger Number</h3><hr><p>Radius: Approximately 210 miles</p>";
     
         for (let i = 0; i < chargerAmount.length; i++) {
-            let color = markerColor(chargerAmount[i]);
-            div.innerHTML +=
-                '<i style="background:' + color + '"></i> ' +
-                chargerAmount[i] + (chargerAmount[i + 1] ? '&ndash;' + chargerAmount[i + 1] + '<br>' : '+');
+            if (i === chargerAmount.length - 1) {
+                div.innerHTML +=
+                    '<i style="background:' + markerColor(chargerAmount[i]) + '"></i> ' +
+                    chargerAmount[i] + '+<br>';
+            } else {
+                div.innerHTML +=
+                    '<i style="background:' + markerColor(chargerAmount[i]) + '"></i> ' +
+                    chargerAmount[i] + (chargerAmount[i + 1] ? '&ndash;' + chargerAmount[i + 1] + '<br>' : '');
+            }
         }
         return div;
     };
